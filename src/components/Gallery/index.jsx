5ef575859131ac48b89886325b464759d1171ba7
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-04-29 18:22:40 -0300

    feat: filter photos via tags

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -2,15 +2,25 @@ import Tags from 'components/Tags';
 import styles from './Gallery.module.scss';
 import Cards from './Cards';
 import photos from './photos.json';
+import { useState } from 'react';
 
 const Gallery = () => {
-  const tags = ['Estrelas', 'Galáxias', 'Luas', 'Planetas'];
+  const [items, setItems] = useState(photos);
+  const tags = ['Todas', ...new Set(photos.map((photo) => photo.tag))];
+
+  const filterPhotos = (tag) => {
+    let photosFound = photos.filter((photo) => photo.tag === tag);
+
+    if (tag === 'Todas') photosFound = photos;
+
+    setItems(photosFound);
+  };
 
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela galeria</h2>
-      <Tags items={tags} />
-      <Cards photos={photos} />
+      <Tags items={tags} filterPhotos={filterPhotos} />
+      <Cards photos={items} />
     </section>
   );
 };
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,12 +1,19 @@
 import styles from './Tags.module.scss';
 
-const Tags = ({ items }) => {
+const Tags = ({ items, filterPhotos }) => {
   return (
     <div className={styles.tags}>
       <p>Filtre por tags:</p>
       <ul className={styles.tags__list}>
         {items.map((item, i) => (
-          <li key={i}>{item}</li>
+          <li
+            key={i}
+            onClick={() => {
+              filterPhotos(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </div>
